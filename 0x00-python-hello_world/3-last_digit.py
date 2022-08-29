727ff5ebd874a1b072dce7905e328331e91f3be0
bsbanotto <4905@holbertonschool.com>
2022-08-29 13:54:20 -0700

    Trying to handle negative numbers better

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
-print("Last digit of {} is {} and is".format(number, last),end = "")
-if last == 0:
-    print(" 0")
-elif last > 5:
-    print(" greater than 5")
-else:
+if number < 0:
+    last = (number % 10) * -1
+    print("Last digit of {} is {} and is".format(number, last), end = "")
     print(" less than 6 and not 0")
+elif number >= 0:
+    last = number % 10
+    print("Last digit of {} is {} and is".format(number, last), end = "")
+    if last == 0:
+        print(" 0")
+    elif last > 5:
+        print(" greater than 5")
+    else:
+        print(" less than 6 and not 0")
