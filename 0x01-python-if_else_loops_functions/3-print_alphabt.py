46d2a54811d1ee5be76966360a42e0f1d19972e5
bsbanotto <4905@holbertonschool.com>
2022-08-29 11:03:56 -0700

    First commit to print alphabet no e or q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for char in range(97, 101):
+    print("{:c}".format(char), end="")
+for char in range(102, 113):
+    print("{:c}".format(char), end="")
+for char in range(114, 123):
+    print("{:c}".format(char), end="")
