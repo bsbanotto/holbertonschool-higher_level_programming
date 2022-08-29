6321a7977442d0c4fd6cdf82ec491ac495483f29
bsbanotto <4905@holbertonschool.com>
2022-08-29 12:46:26 -0700

    Read the instructions this time

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for char in range(97, 101):
-    print("{:c}".format(char), end="")
-for char in range(102, 113):
-    print("{:c}".format(char), end="")
-for char in range(114, 123):
-    print("{:c}".format(char), end="")
+for x in range(97, 123):
+    if x == 101:
+        continue
+    if x == 113:
+        continue
+    print("{:c}".format(x), end="")
