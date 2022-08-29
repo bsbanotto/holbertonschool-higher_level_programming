99eaef09a876fcd533ef50ef2516140458be163f
bsbanotto <4905@holbertonschool.com>
2022-08-29 13:09:18 -0700

    First commit of last digit task

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10
+print("Last digit of {} is {} and is".format(number, last),end = "")
+if last == 0:
+    print(" 0")
+elif last > 5:
+    print(" greater than 5")
+else:
+    print(" less than 6 and not 0")
