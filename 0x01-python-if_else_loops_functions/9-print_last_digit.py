3b78211a497453ff1e2cfe2894dff58bb8edfa7c
bsbanotto <4905@holbertonschool.com>
2022-08-30 07:16:08 -0700

    First commit of 9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        print("{}".format(number % 10), end = "")
+        return number % 10
+    else:
+        number *= -1
+        print("{}".format(number % 10), end = "")
+        return number % 10
