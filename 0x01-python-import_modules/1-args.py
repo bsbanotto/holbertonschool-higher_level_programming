c8fddeee40dc7977d77134f4c849b6ddcc0b2b5e
root <ben.sbanotto@gmail.com>
2022-08-31 15:14:52 +0000

    Made so it runs only as a script

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 1:
-    print("0 arguments.")
 
-if len(argv) == 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments.")
 
-if len(argv) > 2:
-    print("{} arguments:".format(len(argv)-1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    if len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+
+    if len(argv) > 2:
+        print("{} arguments:".format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
