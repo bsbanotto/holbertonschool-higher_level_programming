207857a39922c53ff4b9c6f4a7b8d8cd770880d9
root <ben.sbanotto@gmail.com>
2022-09-01 18:36:23 +0000

    Added empty string in last print

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for value in row:
                 print("{:d}".format(value), end="")
-            print()
+            print("")
