2418467aa98ce93b38a9a973385b75a7c0f80447
root <ben.sbanotto@gmail.com>
2022-09-01 18:46:40 +0000

    Rewrote to add space after each integer

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            for value in row:
-                print("{:d}".format(value), end="")
-            print("")
+    for i in range(len(matrix)):
+        if i != 0:
+            print()
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+    print("")
