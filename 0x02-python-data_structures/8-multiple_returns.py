d38f15cc727c5f17b1150589ac22e9d24d6af6ac
root <ben.sbanotto@gmail.com>
2022-09-01 19:25:39 +0000

    First commit for task 8

---
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,16 @@
+#!/bin/bash/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        length = 0
+    else:
+        length = len(sentence)
+#    print(length)
+
+    if len(sentence) == 0:
+        first = None
+    else:
+        first = sentence[0]
+#    print(first)
+
+    ret_pair = length, first
+    return ret_pair
