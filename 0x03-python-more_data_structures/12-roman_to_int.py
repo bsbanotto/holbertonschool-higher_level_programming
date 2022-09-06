bce702ac62ecf813f621f15afef4101bc838bdbb
root <ben.sbanotto@gmail.com>
2022-09-06 23:15:29 +0000

    Committing with if guards and list of roman numerals for partial credit

---
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) is str:
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                          'C': 100, 'D': 500, 'M': 1000}
+        print(roman)
+
+    else:
+        return None
