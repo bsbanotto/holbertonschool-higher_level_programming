6b73749c9c6d6e8f3d5c8cca26adef548982f82b
root <ben.sbanotto@gmail.com>
2022-09-06 23:40:46 +0000

    Commit task 12 with logic and long variable names

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    answer = 0
+    give_or_take = 0
     if roman_string and type(roman_string) is str:
         roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                           'C': 100, 'D': 500, 'M': 1000}
-        print(roman)
+        for numeral in range(len(roman_string) - 1, -1, -1):
+            if roman_numerals[roman_string[numeral]] >= give_or_take:
+                answer += roman_numerals[roman_string[numeral]]
+            else:
+                answer -= roman_numerals[roman_string[numeral]]
+            give_or_take = roman_numerals[roman_string[numeral]]
+        return answer
 
     else:
         return None
