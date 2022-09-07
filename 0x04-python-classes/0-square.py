5b5c89ed4be0dfa2e2d34e4be91bd6d1a8fc0c5e
root <ben.sbanotto@gmail.com>
2022-09-07 13:03:40 +0000

    First commit of README and Task 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """This class defines a square based on size
+    Size is the length of any one side
+    Size will be a private attribute
+    """
+    def __init__(self, size="0"):
+        """This function controls the size of the
+        square
+        """
+        self.__size = size
