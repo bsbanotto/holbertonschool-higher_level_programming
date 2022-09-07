a46d06bd77fe56c20c8cdc08a1d66662739df916
root <ben.sbanotto@gmail.com>
2022-09-07 13:29:55 +0000

    First commit task 1. Changed size="0" to size=0 in both init

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -8,7 +8,7 @@ class Square:
     Size is the length of any one side
     Size will be a private attribute
     """
-    def __init__(self, size="0"):
+    def __init__(self, size=0):
         """This method initializes the square
         """
         self.__size = size
