8f38e8da2cc92b419da37ce003ccf70e12cbd3ed
bsbanotto <4905@holbertonschool.com>
2022-09-09 12:40:36 -0700

    First commit for Say My Name task

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This will repeate given names back to us
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """First name must be privided as a string
+       Last name, when provided, must be a string
+       If no last name is provided, it will be empty string
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    
+    if len(last_name) > 0:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
