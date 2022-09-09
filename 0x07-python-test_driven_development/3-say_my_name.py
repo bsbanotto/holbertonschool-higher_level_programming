a2bc3bab4d30498fcb1cebd014cef35cb85c1376
bsbanotto <4905@holbertonschool.com>
2022-09-09 12:41:23 -0700

    pep8 fixes for say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    
+
     if len(last_name) > 0:
         print("My name is {} {}".format(first_name, last_name))
     else:
