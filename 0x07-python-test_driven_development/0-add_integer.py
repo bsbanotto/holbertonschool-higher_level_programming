7e7331fc4ab79af665c1b6d87607602e372a99b6
bsbanotto <4905@holbertonschool.com>
2022-09-09 09:05:00 -0700

    Committing files for task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns the sum of two integers, or floats typecasted as int
+"""
+
+
+def add_integer(a, b=98):
+    """Math takes place here"""
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return(a + b)
