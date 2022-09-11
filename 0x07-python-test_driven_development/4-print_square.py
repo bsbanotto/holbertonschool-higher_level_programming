97fda885605afd98c512da83a7dc39b85328d4e9
bsbanotto <4905@holbertonschool.com>
2022-09-11 14:39:15 -0700

    First commit for print square task

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This program prints a square using the # symbol at a give size
+"""
+
+
+def print_square(size):
+    """This function prints the square with error handling
+    """
+
+    if size < 0 and type(size) is float:
+        raise TypeError("size must be an integer")
+    
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0 and type(size) is int:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(0, size):
+        if i > 0:
+            print()
+        for j in range(0, size):
+            print('#', end="")
+    print()
