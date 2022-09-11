8209e4fe740321f1b89c980d15f0b139f4cc6e0c
bsbanotto <4905@holbertonschool.com>
2022-09-11 14:11:17 -0700

    If tests for matrix types changed from or to and, cleaned up test file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,23 +11,32 @@ def matrix_divided(matrix, div):
     New matrix should be returned
     Each row of the matrix should be the same size
     """
+
+    """Check that divisor is not zero"""
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
+    """Chech that divisor is present"""
     if div is None:
         raise TypeError("matrix_divided() missing 1\
                         required positional argument: 'div'")
-    if type(div) is int:
-        div = float(div)
-    if type(div) is not float:
+
+    """Check that divisor is a number"""
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    for row in range(0, len(matrix)):
 
-        for num in range(0, len(matrix[row])):
+    """Check that each row of the matrix is the same size"""
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
 
-            if type(num) is int:
-                num = float(num)
-            if type(num) is not float:
-                raise TypeError("matrix must be a matrix \(list of lists)\
-                                of integers/floats")
+    """Check that each value in the matrix is a number"""
+    for row in matrix:
+        for num in row:
+            if type(num) is not int and type(num) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
 
-    return[[num/div for num in row] for row in matrix]
+    """Finally, return a new matrix"""
+    return[[round(num/div, 2) for num in row] for row in matrix]
