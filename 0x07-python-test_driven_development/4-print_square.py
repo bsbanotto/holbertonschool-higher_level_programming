8386c0b7de41d90cb7b34c54a9220ea84351d49a
bsbanotto <4905@holbertonschool.com>
2022-09-12 12:35:52 -0700

    Cleaned up code so I did not have 4 TypeErrors

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,20 +7,11 @@ def print_square(size):
     """This function prints the square with error handling
     """
 
-    if type(size) is str:
-        raise TypeError("size must be an integer")
-
-    if size is None:
-        raise TypeError("size must be an integer")
-
-    if size < 0:
-        if type(size) is float:
-            raise TypeError("size must be an integer")
-        if type(size) is int:
-            raise ValueError("size must be >= 0")
-
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for i in range(0, size):
         if i > 0:
