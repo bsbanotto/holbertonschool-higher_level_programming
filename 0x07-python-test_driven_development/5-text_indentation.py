bca37c20606c0381f488278227f682c8085ea5ef
bsbanotto <4905@holbertonschool.com>
2022-09-12 07:31:54 -0700

    First commits for text indentation task

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Inserts two new lines after special characters . ? :
+"""
+
+def text_indentation(text):
+    """Testing first for valid input to function, then printing
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    text_period = text.replace(". ", ".\n\n")
+    text_question = text_period.replace("? ", "?\n\n")
+    text_colon = text_question.replace(": ", ":\n\n")
+
+    print(text_colon)
