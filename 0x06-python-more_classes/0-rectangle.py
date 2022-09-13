3a02ab5dfc232cf02ca32b3e93a5e569e9b40d3c
root <ben.sbanotto@gmail.com>
2022-09-13 16:53:15 +0000

    Copy-paste error in TypeError for height

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
         """ Sets the height of self rectangle
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
