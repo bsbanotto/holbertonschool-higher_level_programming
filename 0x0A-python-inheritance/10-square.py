6d251672a5fa3d0268464e7d05e570d249ad8874
bsbanotto <4905@holbertonschool.com>
2022-09-15 13:34:26 -0700

    Cleaned up documentation, replaced rectangle with square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Module for rectangle class that inherits from BaseGeometry
+"""Module for Square class that inherits from Rectangle
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """This is my Rectangle specific class
+    """This is my Square specific class
     """
     def __init__(self, size):
-        """initializes my Rectangle
+        """initializes my Square
         """
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """Returns the area of the self rectangle
+        """Returns the area of the self Square
         """
         return(self.__size * self.__size)
 
     def __str__(self):
         """Returns the human readable string representation of self
+        Still says [Rectangle] for task requirements
         """
         return("[Rectangle] {}/{}".format(self.__size, self.__size))
