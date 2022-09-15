f461a7da7c83bef28279294de0b2abe4e00c526b
bsbanotto <4905@holbertonschool.com>
2022-09-15 12:05:06 -0700

    Using issubclass builtin

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,4 @@ def inherits_from(obj, a_class):
     """ This function returns True if object is an instance of a class
     that inherited from the specified class
     """
-    if type(obj) == a_class:
-        return False
-    return True
+    return issubclass(type(obj), a_class)
