3259b2a606226708fdbfd4f4686ec395875d9e23
bsbanotto <4905@holbertonschool.com>
2022-09-19 09:43:25 -0700

    Changed from append to write for task 7 attempts

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """This function writes my_obj to filenmame using JSON representation
     """
-    with open(filename, 'a', encoding="UTF8") as newFile:
+    with open(filename, 'w', encoding="UTF8") as newFile:
         return json.dump(my_obj, newFile)
