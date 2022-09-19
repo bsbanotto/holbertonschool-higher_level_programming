d2f7332e6d43d685ba145e441fcd2d5753fa7f2d
bsbanotto <4905@holbertonschool.com>
2022-09-19 09:42:11 -0700

    Fixed pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Script that add all arguments to a Python list
 """
+import sys
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
-import sys
+
 
 try:
     pythonList = load("add_item.json")
