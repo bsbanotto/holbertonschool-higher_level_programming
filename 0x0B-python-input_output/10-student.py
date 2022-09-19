13df124e3372f5cd357bb7fec7cbd50a053d512c
bsbanotto <4905@holbertonschool.com>
2022-09-19 11:44:45 -0700

    First commit task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines a student class
+"""
+
+
+class Student():
+    """Student attributes are defined here
+    """
+    def __init__(self, first_name, last_name, age):
+        """Inistantiate the Student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Send the Student class to json
+        """
+        if attrs is None:
+            return vars(self)
+        studentDictionary = {}
+        for i in attrs:
+            try:
+                studentDictionary[i] = vars(self)[i]
+            except:
+                Exception
+            pass
+        return studentDictionary
