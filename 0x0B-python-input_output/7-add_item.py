d8abcd6aa56f25372ec088ebd4d327d7db935f14
bsbanotto <4905@holbertonschool.com>
2022-09-19 09:41:16 -0700

    First attempt task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that add all arguments to a Python list
+"""
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+import sys
+
+try:
+    pythonList = load("add_item.json")
+
+except(TypeError, FileNotFoundError):
+    pythonList = []
+
+for i in sys.argv[1:]:
+    pythonList.append(i)
+
+save(pythonList, "add_item.json")
