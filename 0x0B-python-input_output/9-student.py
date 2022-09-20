195dd1caf2dbbbf7ff18ccfe319b083df4ec5c31
bsbanotto <4905@holbertonschool.com>
2022-09-20 06:55:54 -0700

    Improved documentation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,13 @@ class Student():
     """Student attributes are defined here
     """
     def __init__(self, first_name, last_name, age):
+        """Inistantiate the Student class
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Send Student to JSON
+        """
         return vars(self)
