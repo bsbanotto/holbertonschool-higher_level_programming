3b30a662b7a20e01cc85c6b3a99bdd5f3fa541b4
bsbanotto <4905@holbertonschool.com>
2022-09-20 06:56:34 -0700

    Committing file to clean up branch

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module will print a Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """This function will print a Pascal's triangle with 'n' rows
+    """
+    if n == 1:
+        return[1]
+    if n == 2:
+        return[1, 1]
+    if n > 2:
+        pass
+    
