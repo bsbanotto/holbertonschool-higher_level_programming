46c02765108458a887488c1d7980043039e08e49
bsbanotto <4905@holbertonschool.com>
2022-09-20 18:28:40 -0700

    Reverting back to something that has been tried already

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,9 +25,7 @@ class Base:
         """
         Method to return the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None:
-            return "[]"
-        elif len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
