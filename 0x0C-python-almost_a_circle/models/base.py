76ffa4e3dff627f21e31d46f4b293541136b21c4
bsbanotto <4905@holbertonschool.com>
2022-09-21 14:52:33 -0700

    Had my try in the wrong spot

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,8 +70,8 @@ class Base:
         """
         This method returns a list of instances of objects
         """
-        with open(cls.__name__+".json", "r", encoding="UTF8") as openedFile:
-            try:
+        try:
+            with open(cls.__name__+".json", "r", encoding="UTF8") as openedFile:
                 file_string = openedFile.read()
                 dlist = []
                 for obj in cls.from_json_string(file_string):
