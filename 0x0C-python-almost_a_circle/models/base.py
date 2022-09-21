89961c26ce8261b5595bae0b69a41ed8109af478
bsbanotto <4905@holbertonschool.com>
2022-09-20 18:23:45 -0700

    Trying a whole new approach, elif runs slow

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
         """
         if list_dictionaries is None:
             return "[]"
-        if len(list_dictionaries) == 0:
+        elif len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
