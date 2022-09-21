12f31816e085259ed52809e11079ecfdc188c835
bsbanotto <4905@holbertonschool.com>
2022-09-20 18:19:50 -0700

    Trying a new approach to get the last check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,6 @@ class Base:
         """
         Method to return the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
+        if list_dictionaries is not None or len(list_dictionaries) > 0:
             return json.dumps(list_dictionaries)
+        return "[]"
