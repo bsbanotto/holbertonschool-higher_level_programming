cc3357880ac04708fbe4bc22eb363c9ac583630a
bsbanotto <4905@holbertonschool.com>
2022-09-22 17:30:07 -0700

    Removed testing from Rectangle and Square, added pep8 to base, cleaned up Base doc testing

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -21,17 +21,17 @@ class test_base(unittest.TestCase):
         """
         Test base module documentation exists
         """
-        self.assertTrue(len(Base.__doc__) >= 1)
+        self.assertTrue(len(__import__('models').base.__doc__) >= 1)
 
     def test_class_docstring(self):
         """
         Test Base class documentation exists
         """
-        self.assertTrue(len(Base.__doc__) >= 1)
+        self.assertTrue(len(__import__('models').base.Base.__doc__) >= 1)
 
     def test_method_docstring(self):
         """
-        Tests base class methods for documentation
+        Tests base methods for documentation
         """
         self.assertTrue(len(Base.__init__.__doc__) >= 1)
         self.assertTrue(len(Base.to_json_string.__doc__) >= 1)
@@ -40,60 +40,39 @@ class test_base(unittest.TestCase):
         self.assertTrue(len(Base.create.__doc__) >= 1)
         self.assertTrue(len(Base.load_from_file.__doc__) >= 1)
 
+    def test_pep8_compliance(self):
+        """
+        Tests to ensure models/base and /tests/test_models/test_base.py
+        are pep8 compliant
+        """
+        file1 = "models/base.py"
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/base.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+        file1 = "models/base.py"
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["tests/test_models/test_base.py"])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
     def test_init(self):
         """
-        Tests that id is created correctly
+        Tests self.id is created correctly
         """
         base1 = Base()
         base2 = Base(2)
         base3 = Base()
         base4 = Base()
+        base5 = Base(-42)
+        base6 = Base(None)
         self.assertEqual(base1.id, 1)
         self.assertEqual(base2.id, 2)
         self.assertEqual(base3.id, 2)
         self.assertEqual(base4.id, 3)
-
-    def test_bad_init(self):
-        """
-        Tests for bad values passed to Base.id
-        """
-        with self.assertRaises(TypeError):
-            Base(1, 12)
-        base5 = Base(None)
-        self.assertEqual(base5.id, 1)
-
-    def test_none_to_json_string(self):
-        """
-        Tests that None passed to_json_string is functioning correctly
-        """
-        json_none = Base.to_json_string(None)
-        self.assertEqual(json_none, "[]")
-        self.assertEqual(type(json_none), str)
-
-    def test_two_object_dict_to_json_string(self):
-        """
-        Tests that two objects go to json file properly
-        """
-        Base.base_nb__objects = 0
-        o1 = {"id": 420, "width": 7, "height": 14, "x": 0, "y": 2}
-        o2 = {"id": 98, "width": 2, "height": 4, "x": 2, "y": 0}
-        json_str = Base.to_json_string([o1, o2])
-        self.assertEqual(type(json_str), str)
-        self.assertEqual(len(json.loads(json_str)), 2)
-
-    def test_two_object_dict_from_json_string(self):
-        """
-        Tests that a two item list functions properly
-        """
-        list_input = [
-            {"id": 420, "width": 7, "height": 14, "x": 0, "y": 2},
-            {"id": 98, "width": 2, "height": 4, "x": 2, "y": 0}
-        ]
-        json_list_input = Base.to_json_string(list_input)
-        list_output = Base.from_json_string(json_list_input)
-        self.assertTrue(type(list_output) is list)
-        self.assertTrue(type(json_list_input) is str)
-        self.assertEqual(len(list_output), 2)
+        self.assertEqual(base5.id, -42)
+        self.assertEqual(base6.id, 4)
 
 if __name__ == '__main__':
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,36 +12,7 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-class test_rectangle(unittest.TestCase):
-    """
-    This class will unittest the Rectangle class
-    """
-    def test_module_docstring(self):
-        """
-        Test rectangle module documentation exists
-        """
-        self.assertTrue(len(Rectangle.__doc__) >= 1)
 
-    def test_class_docstring(self):
-        """
-        Test Rectangle class documentation exists
-        """
-        self.assertTrue(len(Rectangle.__doc__) >= 1)
-
-    def test_method_docstring(self):
-        """
-        Tests base class methods for documentation
-        """
-        self.assertTrue(len(Rectangle.__init__.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.width.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.height.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.x.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.y.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.area.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.display.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.__str__.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.update.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.to_dictionary.__doc__) >= 1)
 
 if __name__ == '__main__':
     unittest.main()
