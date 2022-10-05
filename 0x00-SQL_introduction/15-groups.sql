d29fe832a688a910988253da15110bbeae646399
root <ben.sbanotto@gmail.com>
2022-10-05 11:31:02 -0700

    Ordering by score now

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records  with the same score in second_table
-SELECT score 'score', COUNT(score) 'number' FROM second_table GROUP BY score;
+SELECT score 'score', COUNT(score) 'number' FROM second_table GROUP BY score ORDER BY score DESC;
