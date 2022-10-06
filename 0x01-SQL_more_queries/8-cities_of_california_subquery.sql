650f4e07a1c9dd31c4d150d4716d7109f1249f21
root <ben.sbanotto@gmail.com>
2022-10-06 11:11:07 -0700

    First commit task 8

---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Script that lists all the cities of California that can be found in the hbtn_0d_usa database
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states NAME == 'California');
