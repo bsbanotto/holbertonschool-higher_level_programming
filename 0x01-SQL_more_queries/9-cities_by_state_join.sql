a694fe6f817caa7b2384a5b8b8f0069d779ec0dd
root <ben.sbanotto@gmail.com>
2022-10-06 11:42:37 -0700

    Tried again not using JOIN

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
     FROM cities, states
-    FULL OUTER JOIN states ON cities.state_id = states.id;
+    WHERE cities.state_id = states.id;
