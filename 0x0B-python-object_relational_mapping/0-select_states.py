ac2a76f7fb608d307e0aa2a86db2a57aacc4dfb2
bsbanotto <ben.sbanotto@gmail.com>
2022-10-18 11:13:12 -0500

    Committing task 0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This python script lists all states from the database hbtn_0e_0_usa"""
+
+
+import MySQLdb
+
+
+def list_states():
+    """This method lists all the states in the table"""
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM state")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+    if __name__ == "__main__":
+        list_states()
