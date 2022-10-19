0fe66855ccb72efbc527fb7c7f5b3d665ea20669
root <ben.sbanotto@gmail.com>
2022-10-19 07:40:08 -0700

    Committing model_state.py

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of base
+"""
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that inherits from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
