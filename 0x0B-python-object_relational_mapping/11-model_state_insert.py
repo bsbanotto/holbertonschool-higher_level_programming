6a4b375871431034c618465d48d3579d71a2a5e7
root <ben.sbanotto@gmail.com>
2022-10-19 11:41:11 -0700

    Committing task 11

---
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This python script lists all states from hbtn_0e_6_usa
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import insert
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+# add in section to add row to table here
+    session.add(State(name="Louisiana"))
+
+# prints out the new state id
+    last_state = session.query(State).filter(State.name == "Louisiana").first()
+    print("{}".format(last_state.id))
+
+    session.commit()
