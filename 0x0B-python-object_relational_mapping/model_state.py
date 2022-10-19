f2383ad3cc685ec501840405121068b5bbfb28b2
root <ben.sbanotto@gmail.com>
2022-10-19 07:54:41 -0700

    Added some extra stuff to id column

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -14,5 +14,5 @@ class State(Base):
     State class that inherits from Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
