dec01d5631a53c8eac8b0737a819598fb12438e2
root <ben.sbanotto@gmail.com>
2022-11-01 08:43:17 -0700

    Added if name = main

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,10 +4,12 @@ This script fetches https://intranet.hbtn.io/status
 """
 from urllib import request
 
-req = request.Request('https://intranet.hbtn.io/status')
-with request.urlopen(req) as page:
-    page = page.read()
-    print("Body Response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode('utf8')))
+
+if __name__ == "__main__":
+    req = request.Request('https://intranet.hbtn.io/status')
+    with request.urlopen(req) as page:
+        page = page.read()
+        print("Body Response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf8')))
