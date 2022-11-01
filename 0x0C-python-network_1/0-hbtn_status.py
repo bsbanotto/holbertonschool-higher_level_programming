feb9393f496dd221c79c3edb4f449e445541ccf5
root <ben.sbanotto@gmail.com>
2022-11-01 13:34:55 -0700

    Testing code with Dave

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,6 +2,18 @@
 """
 This script fetches https://intranet.hbtn.io/status
 """
+# from urllib import request
+
+
+# if __name__ == "__main__":
+#     req = request.Request('https://intranet.hbtn.io/status')
+#     with request.urlopen(req) as page:
+#         page = page.read()
+#         print("Body response:")
+#         print("\t- type: {}".format(type(page)))
+#         print("\t- content: {}".format(page))
+#         print("\t- utf8 content: {}".format(page.decode('utf8')))
+
 from urllib import request
 
 
