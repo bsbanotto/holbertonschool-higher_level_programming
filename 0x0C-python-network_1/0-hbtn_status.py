47c7a801b6c7ec14088b192312af709a26ce26a4
root <ben.sbanotto@gmail.com>
2022-11-01 13:28:54 -0700

    Testing code with Dave

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     req = request.Request('https://intranet.hbtn.io/status')
     with request.urlopen(req) as page:
         page = page.read()
-        print("Body response:")
+        print("Body Response:")
         print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
         print("\t- utf8 content: {}".format(page.decode('utf8')))
