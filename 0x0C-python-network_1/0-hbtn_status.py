05237e890233932d22a97bb80618b649909572d9
root <ben.sbanotto@gmail.com>
2022-11-01 20:48:21 +0000

    Had some real git issues there

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,18 +2,6 @@
 """
 This script fetches https://intranet.hbtn.io/status
 """
-# from urllib import request
-
-
-# if __name__ == "__main__":
-#     req = request.Request('https://intranet.hbtn.io/status')
-#     with request.urlopen(req) as page:
-#         page = page.read()
-#         print("Body response:")
-#         print("\t- type: {}".format(type(page)))
-#         print("\t- content: {}".format(page))
-#         print("\t- utf8 content: {}".format(page.decode('utf8')))
-
 from urllib import request
 
 
@@ -21,7 +9,7 @@ if __name__ == "__main__":
     req = request.Request('https://intranet.hbtn.io/status')
     with request.urlopen(req) as page:
         page = page.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
         print("\t- utf8 content: {}".format(page.decode('utf8')))
