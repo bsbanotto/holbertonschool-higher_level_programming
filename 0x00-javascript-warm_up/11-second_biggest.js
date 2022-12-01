3f955d82c6b69686953e224a0c6cb9d712ac0db7
root <ben.sbanotto@gmail.com>
2022-12-01 13:20:00 -0800

    Commit for task 11

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const numArray = [];
+for (let i = 2; i < process.argv.length; i++) {
+  numArray.push(parseInt(process.argv[i]));
+}
+// console.log(numArray);
+let biggest = Number.MIN_VALUE;
+let secondBiggest = Number.MIN_VALUE;
+
+if (numArray.length < 2) {
+  console.log(0);
+}
+
+for (let i = 0; i < numArray.length; i++) {
+  if (numArray[i] > biggest) {
+    secondBiggest = biggest;
+    biggest = numArray[i];
+  } else if (numArray[i] > secondBiggest && numArray[i] !== biggest) {
+    secondBiggest = numArray[i];
+  }
+}
+console.log(secondBiggest);
