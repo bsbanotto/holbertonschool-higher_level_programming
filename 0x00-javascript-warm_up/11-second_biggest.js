ae9bcc2d06d8fdec43b31ff78a710299ff11f802
root <ben.sbanotto@gmail.com>
2022-12-01 13:23:04 -0800

    Fixed where biggest and secondBiggest were declared

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -4,19 +4,19 @@ for (let i = 2; i < process.argv.length; i++) {
   numArray.push(parseInt(process.argv[i]));
 }
 // console.log(numArray);
-let biggest = Number.MIN_VALUE;
-let secondBiggest = Number.MIN_VALUE;
 
 if (numArray.length < 2) {
   console.log(0);
-}
-
-for (let i = 0; i < numArray.length; i++) {
-  if (numArray[i] > biggest) {
-    secondBiggest = biggest;
-    biggest = numArray[i];
-  } else if (numArray[i] > secondBiggest && numArray[i] !== biggest) {
-    secondBiggest = numArray[i];
+} else {
+  let biggest = Number.MIN_VALUE;
+  let secondBiggest = Number.MIN_VALUE;
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] > biggest) {
+      secondBiggest = biggest;
+      biggest = numArray[i];
+    } else if (numArray[i] > secondBiggest && numArray[i] !== biggest) {
+      secondBiggest = numArray[i];
+    }
   }
+  console.log(secondBiggest);
 }
-console.log(secondBiggest);
