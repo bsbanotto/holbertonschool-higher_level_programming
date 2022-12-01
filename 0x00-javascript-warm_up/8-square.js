ea2a5afa17bffc80cd473e298e884d754cb34b4f
root <ben.sbanotto@gmail.com>
2022-12-01 11:26:46 -0800

    Trying 8 without console.log

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -3,7 +3,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 }
 const squareSize = process.argv[2];
-// console.log(squareSize);
 for (let i = 0; i < squareSize; i++) {
   process.stdout.write('X');
   for (let j = 1; j < squareSize; j++) {
