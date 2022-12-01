530ee65678188aef477cf3a710169daa7ac8226b
root <ben.sbanotto@gmail.com>
2022-12-01 10:31:44 -0800

    spelled number wrong

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ if (isNaN(parseInt(process.argv[2]))) {
 } else if (process.argv.length < 3) {
   console.log('Not a number');
 } else {
-  console.log('My numner: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
