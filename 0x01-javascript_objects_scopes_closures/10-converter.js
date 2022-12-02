7d4e21c1faa49640c1b13742e3f53a5f7a50c24a
bsbanotto <ben.sbanotto@gmail.com>
2022-12-02 13:50:49 -0600

    Committing task 10 with lots of commtents, but not working

--- 0x01-javascript_objects_scopes_closures/10-converter.js
+++ 0x01-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,29 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return number.toString(base);
+  return toString(base);
 };
-// I know that if I do number.toString(base) I'll get the correct answer
-// I don't know how to get number where I need it
-// Test file:
-// myConverter = converter(16);
 
-// console.log(myConverter(2));
-// console.log(myConverter(12));
-// console.log(myConverter(89));
+/*
+I know that if I do numberObject.toString(base) I'll get the correct answer
+I don't know how to get number where I need it
+https://stackoverflow.com/questions/1337419/how-do-you-convert-numbers-between-different-bases-in-javascript
+Test file:
+myConverter = converter(16);
 
-// In the above, 16 is base. That's easy to get
-// in the console.log(myConverter(number))
-// How do I get that number infront of my toString(base);
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
+In the above, 16 is base. That's easy to get, it's directly passed
+in the console.log(myConverter(numberObject))
+How do I get that infront of my toString(base);
+*/
+
+/* This code works
+How the hell does `return function (numberObject)` get the integer from myConverter???????
+exports.converter = function (base) {
+  return function (numberObject) {
+    return numberObject.toString(base)
+  }
+};
+*/
