222bb62142b32d8b5688714a2f553b370d15b3fb
bsbanotto <ben.sbanotto@gmail.com>
2022-12-02 13:42:24 -0600

    will not work, but has comments

--- 0x01-javascript_objects_scopes_closures/10-converter.js
+++ 0x01-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return toString(base);
+  return number.toString(base);
 };
+// I know that if I do number.toString(base) I'll get the correct answer
+// I don't know how to get number where I need it
+// Test file:
+// myConverter = converter(16);
+
+// console.log(myConverter(2));
+// console.log(myConverter(12));
+// console.log(myConverter(89));
+
+// In the above, 16 is base. That's easy to get
+// in the console.log(myConverter(number))
+// How do I get that number infront of my toString(base);
