afe1cbdcbcb64c2e24c4312924986e4df91f4941
bsbanotto <ben.sbanotto@gmail.com>
2022-12-02 10:49:12 -0600

    Commit for task 4

---
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = parseInt(w);
+      this.height = parseInt(h);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const placeHolder = this.width;
+    this.width = this.height;
+    this.height = placeHolder;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
