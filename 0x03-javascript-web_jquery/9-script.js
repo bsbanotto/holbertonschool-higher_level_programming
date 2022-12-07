cbbf5dc5d797e56562468f2babf9e7e8a2125123
bsbanotto <ben.sbanotto@gmail.com>
2022-12-07 14:39:07 -0600

    Cleaning up commit

--- 0x03-javascript-web_jquery/9-script.js
+++ 0x03-javascript-web_jquery/9-script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   $(document).ready(function () {
     // const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
     // Above url redirects to the one that is not commented out below
-    // When we try this one, we don't get a response, but we do with the redirected url
+    // When we try this one, we don't get a response, but we do with the redirected
     const url = 'https://stefanbohacek.com/hellosalut/?lang=fr';
     $.get(url, function (data) {
       $('#hello').text(data.hello);
